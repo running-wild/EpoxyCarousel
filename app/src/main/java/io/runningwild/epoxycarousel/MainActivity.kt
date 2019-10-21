28c061844878ddb488dc773558c53fb6ca700ad5
Yolanda <yolanda@runningwild.io>
2019-10-21 22:50:18 +0100

    Main logic

--- app/src/main/java/io/runningwild/epoxycarousel/MainActivity.kt
+++ app/src/main/java/io/runningwild/epoxycarousel/MainActivity.kt
@@ -1,12 +1,32 @@
 package io.runningwild.epoxycarousel
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.SnapHelper
+import com.airbnb.epoxy.Carousel
+import com.airbnb.epoxy.EpoxyVisibilityTracker
+import io.runningwild.epoxycarousel.controller.ViewPagerController
+import io.runningwild.epoxycarousel.controller.data.ViewPagerData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager.setController(ViewPagerController().also {
+            it.setData(ViewPagerData(resources.getStringArray(R.array.view_pager_items), 0))
+        })
+
+        // Attach visibility tracker to the RecyclerView to enable visibility events.
+        EpoxyVisibilityTracker().attach(viewPager)
+        Carousel.setDefaultGlobalSnapHelperFactory(object : Carousel.SnapHelperFactory() {
+            override fun buildSnapHelper(context: Context?): SnapHelper {
+                return PagerSnapHelper()
+            }
+        })
     }
 }
